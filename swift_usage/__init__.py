4cddb1166d612e9d19ca01309df92e9ed87f6c12
Will Stevens <wstevens@cloudops.com>
2012-04-12 12:46:41 -0400

    updated the version to 0.0.5

--- swift_usage/__init__.py
+++ swift_usage/__init__.py
@@ -1,4 +1,4 @@
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (0, 0, 4)
+version_info = (0, 0, 5)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
